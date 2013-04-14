12f08fef1938cc6c23572a9440f3f2c218c5f837
Larry Garfield <larry@garfieldtech.com>
2013-04-14 18:58:14 -0500

    Add a lot more tests.

--- tests/ApiProblemTest.php
+++ tests/ApiProblemTest.php
@@ -17,5 +17,54 @@ class ApiProblemTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("URI", $problem->getProblemType());
     }
 
+    public function testSimpleExtraProperty()
+    {
+        $problem = new ApiProblem('Title', 'URI');
+
+        $problem['sir'] = 'Gir';
+        $this->assertEquals('Gir', $problem['sir']);
+
+        unset($problem['sir']);
+        $this->assertNull($problem['sir']);
+    }
+
+    public function testComplexExtraProperty()
+    {
+        $problem = new ApiProblem('Title', 'URI');
+
+        $problem['irken']['invader'] = 'Zim';
+        $this->assertEquals('Zim', $problem['irken']['invader']);
+    }
+
+    public function testSimpleJsonCompile()
+    {
+        $problem = new ApiProblem('Title', 'URI');
+
+        $json = $problem->asJson();
+        $result = json_decode($json, true);
+
+        $this->assertArrayHasKey('title', $result);
+        $this->assertEquals('Title', $result['title']);
+        $this->assertArrayHasKey('problemType', $result);
+        $this->assertEquals('URI', $result['problemType']);
+    }
+
+    public function testExtraPropertyJsonCompile()
+    {
+        $problem = new ApiProblem('Title', 'URI');
+        $problem['sir'] = 'Gir';
+        $problem['irken']['invader'] = 'Zim';
+
+        $json = $problem->asJson();
+        $result = json_decode($json, true);
+
+        $this->assertArrayHasKey('sir', $result);
+        $this->assertEquals('Gir', $result['sir']);
+        $this->assertArrayHasKey('irken', $result);
+        $this->assertArrayHasKey('invader', $result['irken']);
+        $this->assertEquals('Zim', $result['irken']['invader']);
+    }
+
+
 }
 
