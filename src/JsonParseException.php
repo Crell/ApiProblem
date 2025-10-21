4474dc3853d71e77a818f90ace82b48ff3f9e94a
Larry Garfield <larry@garfieldtech.com>
2025-10-20 20:33:52 -0500

    Fix implicit nullability.

--- src/JsonParseException.php
+++ src/JsonParseException.php
@@ -12,7 +12,7 @@ class JsonParseException extends JsonException
      * This version forces a string for $failedValue, as that's the only thing that
      * could fail to parse, since that's all you can even try to parse.
      */
-    public function __construct(string $message = '', int $code = 0, \Throwable $previous = null, string $failedValue = '')
+    public function __construct(string $message = '', int $code = 0, ?\Throwable $previous = null, string $failedValue = '')
     {
         parent::__construct($message, $code, $previous);
         $this->setFailedValue($failedValue);
